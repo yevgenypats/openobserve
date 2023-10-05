57d673c065f49976449e2b38c1d41257433c5e18
omkark06 <omkar@zinclabs.io>
2023-10-05 12:33:35 +0530

    fix: enabled datadog logs

--- web/src/composables/activityTracking2.ts
+++ web/src/composables/activityTracking2.ts
@@ -13,57 +13,62 @@
 //  limitations under the License.
 
 import { useStore } from "vuex";
-import { datadogRum as openobserveRum } from "@datadog/browser-rum";
+import { datadogRum } from "@datadog/browser-rum";
+import { datadogLogs } from "@datadog/browser-logs";
 import "@datadog/browser-rum/bundle/datadog-rum";
 import config from "@/aws-exports";
 
 const userActivityTracking = () => {
   const store = useStore();
   const initializeTracking = () => {
-    openobserveRum.init({
+    datadogRum.init({
       applicationId: "d2017c95-c1dd-4ad0-851b-db25947f406e",
       clientToken: "pube705971a05eed4d79c5f2e2362837397",
       site: "us5.datadoghq.com",
       service: "openobserve",
       env: config.environment,
-      // Specify a version number to identify the deployed version of your application in Datadog
-      // version: '1.0.0',
-      forwardErrorsToLogs: true,
-      forwardConsoleLogs: "all",
-      insecureHTTP: true,
-      apiVersion: "v1",
       sessionSampleRate: 100,
       premiumSampleRate: 100,
       trackUserInteractions: true,
+      trackResources: true,
+      trackLongTasks: true,
       defaultPrivacyLevel: "mask-user-input",
     });
 
-    openobserveRum.startSessionReplayRecording();
+    datadogLogs.init({
+      clientToken: "pube705971a05eed4d79c5f2e2362837397",
+      site: "us5.datadoghq.com",
+      forwardErrorsToLogs: true,
+      sessionSampleRate: 100,
+      service: "openobserve",
+    });
+
+    datadogRum.startSessionReplayRecording();
   };
 
   const setUser = (userInfo: any) => {
-    openobserveRum.setUser({
+    datadogRum.setUser({
       name: userInfo.given_name + " " + userInfo.family_name,
       email: userInfo.email,
     });
   };
 
   const setUserProperty = (key: string, value: any) => {
-    openobserveRum.removeUserProperty(key);
-    openobserveRum.setUserProperty(key, value);
+    datadogRum.removeUserProperty(key);
+    datadogRum.setUserProperty(key, value);
   };
 
   const setGlobalContext = (obj: {}) => {
-    openobserveRum.setGlobalContext(obj);
+    datadogRum.setGlobalContext(obj);
   };
 
   const setGlobalContextProperty = (key: string, value: any) => {
-    openobserveRum.removeGlobalContextProperty(key);
-    openobserveRum.setGlobalContextProperty(key, value);
+    datadogRum.removeGlobalContextProperty(key);
+    datadogRum.setGlobalContextProperty(key, value);
   };
 
   const catchError = (error: any) => {
-    openobserveRum.addError(error, {
+    datadogRum.addError(error, {
       pageURL: window.location.href,
     });
   };
