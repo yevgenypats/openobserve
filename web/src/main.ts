8056b1b161af2effde70b9b00e899115d2c41137
omkark06 <omkar@zinclabs.io>
2023-10-31 11:17:26 +0530

    fix: added privacy level as allow

--- web/src/main.ts
+++ web/src/main.ts
@@ -148,6 +148,7 @@ const getConfig = async () => {
       trackLongTasks: true,
       trackUserInteractions: true,
       version: "v1",
+      defaultPrivacyLevel: "allow",
     });
 
     datadogLogs.init({
