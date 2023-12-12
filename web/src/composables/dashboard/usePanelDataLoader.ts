2927cfc080c77f51e07e2d96ee3916ca9b97d41f
ktx-abhay <abhay.padamani@kiara.tech>
2023-12-12 17:58:00 +0530

    fix: initial call issue

--- web/src/composables/dashboard/usePanelDataLoader.ts
+++ web/src/composables/dashboard/usePanelDataLoader.ts
@@ -765,6 +765,15 @@ console.log("inside watch variablesData");
           return true;
         }
 
+        // if no adhoc variables used, then allow to run the query 
+          if (
+            !variablesData.value?.values
+              ?.filter((it: any) => it.type === "dynamic_filters")
+              ?.map((it: any) => it?.value)
+              .flat().length
+          ) {
+            return true;
+          }
 
         if (!adHocVariables.length) {
           console.log(
